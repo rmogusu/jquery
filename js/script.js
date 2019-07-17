920a16b669966cc52ff5f23ebfa9cceaf9addcca
mrsfreddy <rmogusu123@gmal.com>
2019-07-17 01:31:57 -0400

    Add jquery folder

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+  jQuery(document).ready(function() {
+    jQuery("h1").click(function() {
+      alert("This is a header.");
+    });
+
+    jQuery("p").click(function() {
+      alert("This is a paragraph.");
+    });
+
+    jQuery("image").click(function() {
+      alert("This is an image.");
+    });
+  });
